0cd542d0028dabddd0b08fd4756db2f50a793e3c
Rahul18A09 <rahulbharda60@gmail.com>
2026-03-31 11:59:25 +0530

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,54 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blogs from "./pages/Blogs";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import SingleBlog from "./components/SingleBlog";
+import AdminLogin from "./pages/admin/AdminLogin";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminCreateBlog from "./pages/admin/AdminCreateBlog";
+import AdminUpdateBlog from "./pages/admin/AdminUpdateBlog";
+import ProtectedRoute from "./components/ProtectedRoute";
+import UserProtectedRoute from "./components/UserProtectedRoute";
+import UserLogin from "./pages/UserLogin";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith('/admin');
+
+  return (
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      {!isAdminRoute && <Navbar/>}
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/blogs/:id" element={<UserProtectedRoute><SingleBlog/></UserProtectedRoute>}/>
+
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+        <Route path="/admin/create-blog" element={<ProtectedRoute><AdminCreateBlog /></ProtectedRoute>} />
+        <Route path="/admin/update-blog/:id" element={<ProtectedRoute><AdminUpdateBlog /></ProtectedRoute>} />
+
+        {/* 404 Route */}
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+       {!isAdminRoute && <Footer/>}
+    </>
+  ); 
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+// react icons
+import {
+  FaBars,
+  FaDribbble,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
+
+function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+  
+  const token = localStorage.getItem("userToken");
+  
+  const handleLogout = () => {
+    localStorage.removeItem("userToken");
+    toast.success("Successfully logged out!");
+    navigate("/login");
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  // navItems
+  const navItems = [
+    { path: "/", link: "Home" },
+    { path: "/services", link: "Services" },
+    { path: "/about", link: "About" },
+    { path: "/blogs", link: "Blogs" },
+    { path: "/contact", link: "Contact" },
+  ];
+  return (
+    <header className="bg-black text-white fixed top-0 left-0 right-0 z-50">
+      <nav className="px-4 py-4 max-w-7xl mx-auto flex justify-between items-center">
+        <a href="/" className="text-xl font-bold text-white">
+          BlogWith<span className="text-orange-500">Rahul</span>
+        </a>
+
+        {/* navItems for lg devices */}
+        <ul className="md:flex gap-12 text-lg hidden">
+          {navItems.map(({ path, link }) => (<li key={path}> <NavLink to={path} className={({ isActive }) => isActive ? "text-orange-500 font-semibold underline underline-offset-4": "text-white"}>{link} </NavLink></li>))}
+        </ul>
+
+        {/* menu icons */}
+
+        <div className="text-white lg:flex gap-4 items-center hidden">
+          <a
+            href="https://www.facebook.com/rahul_0918"
+            className="hover:text-orange-500"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/rahul-bharada"
+            className="hover:text-orange-500"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://twitter.com/rahul_0918A"
+            className="hover:text-orange-500"
+          >
+            <FaTwitter />
+          </a>
+          {token ? (
+            <button
+              onClick={handleLogout}
+              className="bg-gray-700 px-6 py-2 font-medium rounded text-white hover:bg-white hover:text-gray-800 transition-all duration-200 ease-in"
+            >
+              Log out
+            </button>
+          ) : (
+            <NavLink
+              to="/login"
+              className="bg-orange-500 px-6 py-2 font-medium rounded text-white hover:bg-white hover:text-orange-500 transition-all duration-200 ease-in"
+            >
+              Log in
+            </NavLink>
+          )}
+        </div>
+
+        {/* mobile menu btn, display on  mobile screen */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="cursor-pointer">
+            {isMenuOpen ? (
+              <FaXmark className="w-5 h-5" />
+            ) : (
+              <FaBars className="w-5 h-5" />
+            )}
+          </button>
+        </div>
+      </nav>
+      {/* meno items only for mobile  */}
+      <div>
+        <ul
+          className={`md:hidden gap-12 text-lg block space-y-4 px-4 py-6 mt-14 bg-white ${isMenuOpen ? "fixed top-0 left-0 w-full transition-all ease-out duration-150" : "hidden"}`}
+        >
+          {navItems.map(({ path, link }) => (
+            <li className="text-black" key={path}>
+              <NavLink onClick={toggleMenu} to={path}>
+                {link}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
